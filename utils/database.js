44cf8d3846a346a4eb07938ce2d6996e425366a6
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2023-12-16 23:16:43 +0600

    completed the project

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  isConnected = true;
+
+  console.log("MongoDB is connected successfully.");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
