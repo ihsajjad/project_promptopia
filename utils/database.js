6d405e51c5b71733e05bacd65f460e0ee63dfd3f
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2023-12-16 23:40:41 +0600

    changed the uri2

--- utils/database.js
+++ utils/database.js
@@ -15,7 +15,7 @@ export const connectToDB = async () => {
   console.log("MongoDB is connected successfully.");
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(`${process.env.MONGODB_URI}`, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
