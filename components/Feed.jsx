d39932348abcd6caacea699b4069bceb6381f4a0
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2023-12-17 12:57:42 +0600

    changed the url

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
-// import PromptCard from "./PromptCard";
-// import PromptCard from "./PromptCard";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
@@ -18,39 +16,41 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   };
   return (
     <div className="prompt_card">
-      <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image
-            src={post?.creator?.image || ""}
-            alt="user_image"
-            width={40}
-            height={40}
-            className="rounded-full object-contain"
-          />
+      {post && (
+        <div className="flex justify-between items-start gap-5">
+          <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+            <Image
+              src={post?.creator?.image || ""}
+              alt="user_image"
+              width={40}
+              height={40}
+              className="rounded-full object-contain"
+            />
 
-          <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">
-              {post?.creator?.username}
-            </h3>
-            <p className="font-inter text-sm text-gray-500">
-              {post?.creator?.email}
-            </p>
+            <div className="flex flex-col">
+              <h3 className="font-satoshi font-semibold text-gray-900">
+                {post?.creator?.username}
+              </h3>
+              <p className="font-inter text-sm text-gray-500">
+                {post?.creator?.email}
+              </p>
+            </div>
+          </div>
+
+          <div className="copy-btn" onClick={handleCopy}>
+            <Image
+              src={
+                copied === post.prompt
+                  ? "/assets/icons/tick.svg"
+                  : "/assets/icons/copy.svg"
+              }
+              alt="copy icon"
+              width={12}
+              height={12}
+            />
           </div>
         </div>
-
-        <div className="copy-btn" onClick={handleCopy}>
-          <Image
-            src={
-              copied === post.prompt
-                ? "/assets/icons/tick.svg"
-                : "/assets/icons/copy.svg"
-            }
-            alt="copy icon"
-            width={12}
-            height={12}
-          />
-        </div>
-      </div>
+      )}
 
       <p className="my-4 text-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
